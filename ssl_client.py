010f63dcdd33966179dd2fcb4304d12cb37b51ad
Florian Hagengruber <github@hagengruber.dev>
2022-12-17 18:39:21 +0100

    First Try to encrypt communication - FAILED

---
+++ ssl_client.py
@@ -0,0 +1,23 @@
+import socket
+import ssl
+
+from tls_server import HOST as SERVER_HOST
+from tls_server import PORT as SERVER_PORT
+
+HOST = "127.0.0.1"
+PORT = 60002
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+client = ssl.wrap_socket(client, keyfile="./key.txt", certfile="./public")
+
+if __name__ == "__main__":
+    client.bind((HOST, PORT))
+    client.connect((SERVER_HOST, SERVER_PORT))
+
+    while True:
+        from time import sleep
+
+        client.send("Hello World!".encode("utf-8"))
+        sleep(1)
